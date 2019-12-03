029ef2ed5fbf2e0b3f40f86d8ccb03a277843ed6
christina kaliora <develop@pirron.one>
2019-12-03 14:04:59 +0100

    finished timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,12 +1,30 @@
-var timeleft = 0;
-var timer = setInterval(function(){
-  let secondTens = document.getElementById('secondTens').innerHTML = 0;
-  let secondOnes = document.getElementById('secondOnes').innerHTML = timeleft;
-  timeleft += 1;
+let counter = 1;
 
-  if(timeleft > 10){
-    clearInterval(timer);
-  }
-  
+let timer = setInterval(() => {
+    document.getElementById('secondTens').innerHTML = 0;
+    document.getElementById('secondOnes').innerHTML = counter;
+    counter += 1;
 
+    if(counter >= 10){
+        document.getElementById('secondTens').innerHTML = 1;
+        document.getElementById('secondOnes').innerHTML = 0;
+        clearInterval(timer);
+    }
 }, 1000);
+
+
+let counterMs = 0;
+let timerMs = setInterval(() => {
+    counterMs += 1;
+
+    if (counterMs >= 10){
+        counterMs = 0;
+        if (counter >= 10 ){
+            clearInterval(timerMs);
+        }
+    }
+
+    document.getElementById('msHundreds').innerHTML = 0;
+    document.getElementById('msTens').innerHTML = counterMs;
+
+}, 100);
