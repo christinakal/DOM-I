575b12037c00fbe7b77e01df17b2bb5f2d851137
christina kaliora <develop@pirron.one>
2019-12-03 18:48:26 +0100

    made the digits red

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,30 +1,48 @@
 const button = document.getElementById('btn');
 const resetButton = document.getElementById('reset-btn');
 
+
+const secondTens = document.getElementById('secondTens');
+const secondOnes = document.getElementById('secondOnes');
+
+const msHundreds = document.getElementById('msHundreds');
+const msTens = document.getElementById('msTens');
+
 button.addEventListener('click', startTimer);
 resetButton.addEventListener('click', resetTimer);
 
-let timer, timerMs;
+let timer;
+let timerMs;
 
 resetTimer();
 
 // TIMER
 function startTimer(){
+    resetTimer();
+
     let counter = 1;
     button.setAttribute("disabled", "true");
     //resetButton.setAttribute("disabled", "true");
 
 
     timer = setInterval(() => {
-        document.getElementById('secondTens').innerHTML = 0;
-        document.getElementById('secondOnes').innerHTML = counter;
+        secondTens.innerHTML = 0;
+        secondOnes.innerHTML = counter;
         counter += 1;
+
+        if ( counter > 8){
+            secondTens.style.color = 'red';
+            secondOnes.style.color = 'red';
+            msHundreds.style.color = 'red';
+            msTens.style.color = 'red';
+        }
+        
     
-        if(counter >= 10){
-            document.getElementById('secondTens').innerHTML = 1;
-            document.getElementById('secondOnes').innerHTML = 0;
+        if(counter >= 11){
+            secondTens.innerHTML = 1;
+            secondOnes.innerHTML = 0;
             button.removeAttribute("disabled");
-            
+
             clearInterval(timer);
         }
     }, 1000);
@@ -36,13 +54,13 @@ function startTimer(){
     
         if (counterMs >= 10){
             counterMs = 0;
-            if (counter >= 10 ){
+            if (counter >= 11 ){
                 clearInterval(timerMs);
             }
         }
     
-        document.getElementById('msHundreds').innerHTML = 0;
-        document.getElementById('msTens').innerHTML = counterMs;
+        msHundreds.innerHTML = 0;
+        msTens.innerHTML = counterMs;
     
     }, 100);
 
@@ -53,11 +71,16 @@ function resetTimer(){
     clearInterval(timer);
     clearInterval(timerMs);
 
+    secondTens.style.color = 'black';
+    secondOnes.style.color = 'black';
+    msHundreds.style.color = 'black';
+    msTens.style.color = 'black';
+
     button.removeAttribute("disabled");
 
-    document.getElementById('secondTens').innerHTML = 0;
-    document.getElementById('secondOnes').innerHTML = 0;
-    document.getElementById('msHundreds').innerHTML = 0;
-    document.getElementById('msTens').innerHTML = 0;
+    secondTens.innerHTML = 0;
+    secondOnes.innerHTML = 0;
+    msHundreds.innerHTML = 0;
+    msTens.innerHTML = 0;
 
 }
