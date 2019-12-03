e5936db45f7b63a72f1242f25811db94ef4f10b9
christina kaliora <develop@pirron.one>
2019-12-03 17:51:53 +0100

    Finished timer and stratch goals

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,30 +1,63 @@
-let counter = 1;
+const button = document.getElementById('btn');
+const resetButton = document.getElementById('reset-btn');
 
-let timer = setInterval(() => {
-    document.getElementById('secondTens').innerHTML = 0;
-    document.getElementById('secondOnes').innerHTML = counter;
-    counter += 1;
+button.addEventListener('click', startTimer);
+resetButton.addEventListener('click', resetTimer);
 
-    if(counter >= 10){
-        document.getElementById('secondTens').innerHTML = 1;
-        document.getElementById('secondOnes').innerHTML = 0;
-        clearInterval(timer);
-    }
-}, 1000);
+let timer, timerMs;
+
+resetTimer();
+
+// TIMER
+function startTimer(){
+    let counter = 1;
+    button.setAttribute("disabled", "true");
+    //resetButton.setAttribute("disabled", "true");
 
 
-let counterMs = 0;
-let timerMs = setInterval(() => {
-    counterMs += 1;
-
-    if (counterMs >= 10){
-        counterMs = 0;
-        if (counter >= 10 ){
-            clearInterval(timerMs);
+    timer = setInterval(() => {
+        document.getElementById('secondTens').innerHTML = 0;
+        document.getElementById('secondOnes').innerHTML = counter;
+        counter += 1;
+    
+        if(counter >= 10){
+            document.getElementById('secondTens').innerHTML = 1;
+            document.getElementById('secondOnes').innerHTML = 0;
+            button.removeAttribute("disabled");
+            
+            clearInterval(timer);
         }
-    }
+    }, 1000);
+    
+    
+    let counterMs = 0;
+    timerMs = setInterval(() => {
+        counterMs += 1;
+    
+        if (counterMs >= 10){
+            counterMs = 0;
+            if (counter >= 10 ){
+                clearInterval(timerMs);
+            }
+        }
+    
+        document.getElementById('msHundreds').innerHTML = 0;
+        document.getElementById('msTens').innerHTML = counterMs;
+    
+    }, 100);
 
+}
+
+
+function resetTimer(){
+    clearInterval(timer);
+    clearInterval(timerMs);
+
+    button.removeAttribute("disabled");
+
+    document.getElementById('secondTens').innerHTML = 0;
+    document.getElementById('secondOnes').innerHTML = 0;
     document.getElementById('msHundreds').innerHTML = 0;
-    document.getElementById('msTens').innerHTML = counterMs;
+    document.getElementById('msTens').innerHTML = 0;
 
-}, 100);
+}
